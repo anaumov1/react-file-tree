b7b37c1cf49ef13b3a16c4a161a8e7cf45dd8526
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-24 15:13:55 -0500

    folders and file structure componenets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 function App () {
-  return 'hello world';
+  return <div>
+  <Folder name="Desktop">
+  <File name="dogs.jpeg"/>
+  <File name="cats.png"/>
+    </Folder>
+
+  <Folder name="Applications"/>
+
+
+
+  </div>
 }
+
+const Folder = (props) => {
+  const borderStyle = { border: '2px solid pink'};
+  return <div style = {borderStyle}>
+    {props.name}
+    <div style = {{ marginLeft: '17px '}}>
+    {props.children}
+    </div>
+    </div>
+};
+
+const File = (props) => {
+  return <div>{props.name}</div>
+};
 
 export default App;
